bfb26232f0ed4e82c3cfebebc8b624dea25808a0
fatii-programmer <160323457+fatii-programmer@users.noreply.github.com>
2024-04-02 12:34:53 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,23 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+
+while(condition){
+let answers = await inquirer.prompt( [
+        {
+            name:'todo',
+            type:'input',
+            message:"what you want to add yoyr todos?"
+        },
+        {
+            name:'addmore',
+            type:'confirm',
+            message:'do you want to admore?',
+            default:"false"
+        },
+        ]);
+
+todos.push(answers.todo);
+condition=answers.addmore
+console.log(todos);
+}
